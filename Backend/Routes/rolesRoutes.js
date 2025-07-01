dbb756a8c1da2c6a688eff053b21bda645659d0c
Vijay Patil <vp0013622@gmail.com>
2025-07-01 10:23:42 +0530

    Update rolesRoutes.js

--- Backend/Routes/rolesRoutes.js
+++ Backend/Routes/rolesRoutes.js
@@ -7,7 +7,7 @@ RolesRouter.post('/create', RoleAuthMiddleware("admin"), Create) //only admin
 RolesRouter.get('/', GetAllRoles)
 RolesRouter.get('/notpublishedroles', RoleAuthMiddleware("admin"), GetAllNotPublishedRoles)//only for admin
 RolesRouter.post('/withparams', RoleAuthMiddleware("admin"), GetAllRolesWithParams)//only for admin
-RolesRouter.get('/:id', RoleAuthMiddleware("admin"), GetRoleById)//only for admin
+RolesRouter.get('/:id', GetRoleById)
 RolesRouter.put('/edit/:id', RoleAuthMiddleware("admin"), Edit)//only for admin
 RolesRouter.delete('/delete/:id', RoleAuthMiddleware("admin"), DeleteById)//only for admin
 export default RolesRouter
