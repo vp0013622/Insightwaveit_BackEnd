b46c5fbda0d8a4f56023f6f1b72dd0b4dfb6a0a3
Vijay Patil <vp0013622@gmail.com>
2025-07-01 19:04:35 +0530

    Update usersRoutes.js

--- Backend/Routes/usersRoutes.js
+++ Backend/Routes/usersRoutes.js
@@ -7,7 +7,7 @@ UsersRouter.post('/register',  RoleAuthMiddleware("admin"), Register) //only adm
 UsersRouter.get('/',  RoleAuthMiddleware("admin"), GetAllUsers)//only for admin
 UsersRouter.get('/notpublishedusers',  RoleAuthMiddleware("admin"), GetAllNotPublishedUsers)//only for admin
 UsersRouter.post('/withparams',  RoleAuthMiddleware("admin"), GetAllUsersWithParams)//only for admin
-UsersRouter.get('/:id',  RoleAuthMiddleware("admin"), GetUserById)//only for admin
+UsersRouter.get('/:id',  RoleAuthMiddleware("admin", "executive", "sales"), GetUserById)//only for admin
 UsersRouter.put('/edit/:id',  RoleAuthMiddleware("admin"), Edit)//only for admin
 UsersRouter.delete('/delete/:id',  RoleAuthMiddleware("admin"), DeleteById)//only for admin
 export default UsersRouter
